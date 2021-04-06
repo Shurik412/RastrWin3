4175f57ac68bca206ea130f9c0eb142c2f80d56e
ohrimenko_ag <shurik412@mail.ru>
2021-04-06 09:50:00 +0300

    Corr file Tables_and_parametrs.py -> Add table "com_dynamic"

--- Equivalence/equivalent_calculation_model.py
+++ Equivalence/equivalent_calculation_model.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from R_modules.load_and_save_file.load_file_rastrwin import load_file
-from R_modules.load_and_save_file.shablons_dir import shablon_file_dynamic, shablon_file_scenario, shablon_file_regime
-from tabl_com_cxema import tb_com_cxema, par_tb_com_cxema
-from R_modules.getting_parameters.get_parameter import GettingParameterAttribute, GettingParameter, GetTableCommonInfo
+from R_modules.load_and_save_file.shablons_dir import shablon_file_regime
+from R_modules.getting_parameters.get_parameter import GetTableCommonInfo
 from R_modules.calculation.dyn_rgm_ekv_calc import SteadyState
-from R_modules.Tables_Parametrs.Tables_and_parametrs import table_node, table_vetv, table_generator, table_name_node, \
-    table_name_vetv, table_name_generator,
 from R_modules.variables.removal_marked_objects import RemoveSelObjects
 from R_modules.calculation.dyn_rgm_ekv_calc import Equivalent
 from R_modules.object_rastr import RASTR
--- R_modules/settings_parametrs/settings_ekv.py
+++ R_modules/settings_parametrs/settings_ekv.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from R_modules.Tables_Parametrs.Tables_and_parametrs import table_name_com_ekviv, table_com_ekviv
+from R_modules.Tables_Parametrs.Tables_and_parametrs import table_name_com_ekviv, table_com_ekviv, table_name_com_regim, \
+    table_com_regim, table_com_dynamics, table_name_com_dynamics
 from R_modules.variables.variable_parametrs import VariableDefRowId
 from R_modules.object_rastr import RASTR
 
@@ -54,6 +55,118 @@ class SetEkviv(VariableDefRowId):
         VariableDefRowId.make_changes(self, column=self.list_key[9], row_id=0, value=smart)
         VariableDefRowId.make_changes(self, column=self.list_key[10], row_id=0, value=zmax)
         VariableDefRowId.make_changes(self, column=self.list_key[11], row_id=0, value=otm_n)
+
+
+class SetRegim(VariableDefRowId):
+    """
+    Класс выставляет параметров настройки "Общие параметры режима"
+
+
+    """
+
+    def __init__(self, rastr_win=RASTR, table=table_name_com_regim, switch_command_line=False):
+        self.rastr_win = rastr_win
+        self.list_key = []
+        for key in table_com_regim.keys():
+            self.list_key.append(key)
+        self.switch_command_line = switch_command_line
+
+        VariableDefRowId.__init__(self, rastr_win=rastr_win, table=table, switch_command_line=switch_command_line)
+
+    def set(self,
+            neb_p=1.000,
+            it_max=500,
+            start=0,
+            flot=1,
+            dv_min=0.5,
+            dv_max=2.000,
+            dd_max=5157,
+            status=0,
+            rr=0,
+            wt='',
+            gen_p=0,
+            method=0,
+            method_ogr=0,
+            print_mode=0,
+            qmax=0,
+            min_x='',
+            calc_tr=0,
+            nag_p=0,
+            rem_breaker=0,
+            gram=0,
+            ctrl_baza=0,
+            itz='',
+            itz_ogr_max='',
+            itz_ogr_min='',
+            min_nodes_in_island=''):
+        VariableDefRowId.make_changes(self, column=self.list_key[0], row_id=0, value=float(neb_p))
+        VariableDefRowId.make_changes(self, column=self.list_key[1], row_id=0, value=it_max)
+        VariableDefRowId.make_changes(self, column=self.list_key[2], row_id=0, value=start)
+        VariableDefRowId.make_changes(self, column=self.list_key[3], row_id=0, value=flot)
+        VariableDefRowId.make_changes(self, column=self.list_key[4], row_id=0, value=float(dv_min))
+        VariableDefRowId.make_changes(self, column=self.list_key[5], row_id=0, value=float(dv_max))
+        VariableDefRowId.make_changes(self, column=self.list_key[6], row_id=0, value=dd_max)
+        VariableDefRowId.make_changes(self, column=self.list_key[7], row_id=0, value=status)
+        VariableDefRowId.make_changes(self, column=self.list_key[8], row_id=0, value=rr)
+        VariableDefRowId.make_changes(self, column=self.list_key[9], row_id=0, value=wt)
+        VariableDefRowId.make_changes(self, column=self.list_key[10], row_id=0, value=gen_p)
+        VariableDefRowId.make_changes(self, column=self.list_key[11], row_id=0, value=method)
+        VariableDefRowId.make_changes(self, column=self.list_key[12], row_id=0, value=method_ogr)
+        VariableDefRowId.make_changes(self, column=self.list_key[13], row_id=0, value=print_mode)
+        VariableDefRowId.make_changes(self, column=self.list_key[14], row_id=0, value=qmax)
+        VariableDefRowId.make_changes(self, column=self.list_key[15], row_id=0, value=min_x)
+        VariableDefRowId.make_changes(self, column=self.list_key[16], row_id=0, value=calc_tr)
+        VariableDefRowId.make_changes(self, column=self.list_key[17], row_id=0, value=nag_p)
+        VariableDefRowId.make_changes(self, column=self.list_key[18], row_id=0, value=rem_breaker)
+        VariableDefRowId.make_changes(self, column=self.list_key[19], row_id=0, value=gram)
+        VariableDefRowId.make_changes(self, column=self.list_key[20], row_id=0, value=ctrl_baza)
+        VariableDefRowId.make_changes(self, column=self.list_key[21], row_id=0, value=itz)
+        VariableDefRowId.make_changes(self, column=self.list_key[22], row_id=0, value=itz_ogr_max)
+        VariableDefRowId.make_changes(self, column=self.list_key[23], row_id=0, value=itz_ogr_min)
+        VariableDefRowId.make_changes(self, column=self.list_key[24], row_id=0, value=min_nodes_in_island)
+
+
+class SetDynamic(VariableDefRowId):
+    """
+    Класс выставляет параметров настройки "Общие данные для расчета динамики"
+    """
+
+    def __init__(self, rastr_win=RASTR, table=table_name_com_dynamics, switch_command_line=False):
+        self.rastr_win = rastr_win
+        self.list_key = []
+        for key in table_com_regim.keys():
+            self.list_key.append(key)
+        self.switch_command_line = switch_command_line
+
+        VariableDefRowId.__init__(self, rastr_win=rastr_win, table=table, switch_command_line=switch_command_line)
+
+    def set(self,
+            neb_p=1.000,
+            it_max=500,
+            start=0,
+            flot=1,
+            dv_min=0.5,
+            dv_max=2.000,
+            dd_max=5157,
+            status=0,
+            rr=0,
+            wt='',
+            gen_p=0,
+            method=0,
+            method_ogr=0,
+            print_mode=0,
+            qmax=0,
+            min_x='',
+            calc_tr=0,
+            nag_p=0,
+            rem_breaker=0,
+            gram=0,
+            ctrl_baza=0,
+            itz='',
+            itz_ogr_max='',
+            itz_ogr_min='',
+            min_nodes_in_island=''):
+        VariableDefRowId.make_changes(self, column=self.list_key[0], row_id=0, value=float(neb_p))
 
 
 if __name__ == '__main__':
