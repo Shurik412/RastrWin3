b02d80383464ee998bcfa99642da8f78ebd8a2f4
ohrimenko_ag <shurik412@mail.ru>
2021-04-23 15:09:04 +0300

    Добавилено внесение районов в ветви

--- Equivalence/na_vetv.py
+++ Equivalence/na_vetv.py
@@ -16,15 +16,30 @@ for i in range(0, 26558):
     ip = get_.get_cell(table=vetv_table,
                        column='ip',
                        row_id=i)
-    row_ny = get_.get_row_node(node_ny=ip)
-    na_node = get_.get_cell(table=node_table,
-                            column='na',
-                            row_id=row_ny)
+    iq = get_.get_cell(table=vetv_table,
+                       column='iq',
+                       row_id=i)
 
-    variable_.make_changes_row(table=vetv_table,
-                               column='na',
-                               row_id=i,
-                               value=na_node)
+    row_ny_ip = get_.get_row_node(node_ny=ip)
+    row_ny_iq = get_.get_row_node(node_ny=iq)
+
+    na_ip = get_.get_cell(table=node_table,
+                          column='na',
+                          row_id=row_ny_ip)
+
+    na_iq = get_.get_cell(table=node_table,
+                          column='na',
+                          row_id=row_ny_iq)
+
+    if na_ip == na_iq:
+        print(f'na_ip={na_ip} == na_iq={na_iq}')
+        variable_.make_changes_row(table=vetv_table,
+                                   column='na',
+                                   row_id=i,
+                                   value=na_ip)
+    else:
+        print(f'ERROR: na_ip={na_ip} != na_iq={na_iq}')
+
 
 save_file(file_path=r'C:\Users\Ohrimenko_AG\Desktop\Test_equiPy\test_123.rg2',
           shablon=shablon_file_regime)
