c10bab38f8348a7549a1c007f56d00ee7c42c318
ohrimenko_ag <shurik412@mail.ru>
2021-04-25 22:41:35 +0300

    Add module DFWIEEE421PSS13.py

---
+++ RastrWinLib/tables/DFWIEEE421PSS13.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# Модуль переменных таблицы  "Системный стабилизатор IEEE" RastrWin3
+
+table: str = 'DFWIEEE421PSS13'
+
+sel: str = 'sel'  # Отметка
+sta: str = 'sta'  # Состояние стабилизатора
+Id: str = 'Id'  # Номер стабилизатора
+Name: str = 'Name'  # Название стабилизатора
+ModelType: str = 'ModelType'  # Модель стабилизатора
+Brand: str = 'Brand'  # Марка стабилизатора
+CustomModel: str = 'CustomModel'  # Модель в конструкторе
+Input1Type: str = 'Input1Type'  # Вход 1 Тип параметра на входе 1
+Input2Type: str = 'Input2Type'  # Вход 2 Тип параметра на входе 2
+
+# Коэффициента
+Ks1: str = 'Ks1'  #
+Ks2: str = 'Ks2'  #
+Ks3: str = 'Ks3'  #
+
+# Времена
+T1: str = 'T1'  #
+T2: str = 'T2'  #
+T3: str = 'T3'  #
+T4: str = 'T4'  #
+T5: str = 'T5'  #
+T6: str = 'T6'  #
+T7: str = 'T7'  #
+T8: str = 'T8'  #
+T9: str = 'T9'  #
+T10: str = 'T10'  #
+T11: str = 'T11'  #
+T12: str = 'T12'  #
+T13: str = 'T13'  #
+
+Tw1: str = 'Tw1'  #
+Tw2: str = 'Tw2'  #
+Tw3: str = 'Tw3'  #
+Tw4: str = 'Tw4'  #
+
+A1: str = 'A1'  #
+A2: str = 'A2'  #
+A3: str = 'A3'  #
+A4: str = 'A4'  #
+A5: str = 'A5'  #
+A6: str = 'A6'  #
+A7: str = 'A7'  #
+A8: str = 'A8'  #
+
+M: str = 'M'  # M Порядок фильтра НЧ
+N: str = 'N'  # N Порядок фильтра
+
+Vstmax: str = 'Vstmax'  # Максимальное ограничение выхода стабилизатора
+Vstmin: str = 'Vstmin'  # Минимальное ограничение выхода стабилизатора
+
+Vsi1max: str = 'Vsi1max'  # Максимальное ограничение 1-го входа стабилизатора
+Vsi1min: str = 'Vsi1min'  # Минимальное ограничение 1-го входа стабилизатора
+
+Vsi2max: str = 'Vsi2max'  # Максимальное ограничение 2-го входа стабилизатора
+Vsi2min: str = 'Vsi2min'  # Минимальное ограничение 2-го входа стабилизатора
