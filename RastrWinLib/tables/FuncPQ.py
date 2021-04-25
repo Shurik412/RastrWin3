87e0b9f76934083d1ad21864b3c6bc2146274002
ohrimenko_ag <shurik412@mail.ru>
2021-04-25 23:53:41 +0300

    Add module FuncPQ.py

---
+++ RastrWinLib/tables/FuncPQ.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Модуль переменных таблицы  "Зависимость Q(P)" RastrWin3
+
+table: str = 'FuncPQ'  # название таблицы
+
+Id: str = 'Id'  # N Номер Х-ки
+P: str = 'P'  # P
+Q: str = 'Q'  # Q
