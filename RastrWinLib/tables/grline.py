a33dd2c77eb99cfcadb2463271652585cce12a86
ohrimenko_ag <shurik412@mail.ru>
2021-04-26 00:12:52 +0300

    Add module grline.py

---
+++ RastrWinLib/tables/grline.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Модуль переменных параметров "Группа линий в сечении" RastrWin3
+
+table: str = 'geline'
+
+sta: str = 'sta'  #
+ns: str = 'ns'  # N_сеч  Номер сечения
+name: str = 'name'  # Имя  Имя узлов
+ip: str = 'ip'  # N_нач  Номер начала
+iq: str = 'iq'  # N_кон  Номер конца
+pl: str = 'pl'  # Pl  Поток
+dv: str = 'dv'  # Кдел  Место замера на линии 0-1
+Ficed: str = 'Ficed'  # Зафиксировать состояние
+
+
+
