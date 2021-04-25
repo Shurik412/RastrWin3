62dd0c5c16ef053dd8eabfbb3e1b30047cb2cd17
ohrimenko_ag <shurik412@mail.ru>
2021-04-25 23:02:59 +0300

    Add module DFWIEEE421PSS4B.py

---
+++ RastrWinLib/tables/DFWIEEE421PSS4B.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# Модуль переменных таблицы  "Системный стабилизатор IEEE PSS4B" RastrWin3
+
+table: str = 'DFWIEEE421PSS4B'  # название таблицы
+
+sel: str = 'sel'  # Отметка
+sta: str = 'sta'  # Состояние стабилизатора
+Id: str = 'Id'  # N стаб Номер стабилизатора
+Name: str = 'Name'  # Название стабилизатора
+ModelType: str = 'ModelType'  # Модель стабилизатора
+Brand: str = 'Brand'  # Марка стабилизатора
+CustomModel: str = 'CustomModel'  # Модель в конструкторе
+Input1Type: str = 'Input1Type'  # Вход 1 Тип параметра на входе 1
+Input2Type: str = 'Input2Type'  # Вход 2 Тип параметра на входе 2
+MBPSS1: bool = False  # Фильтр MB-PSS на входе 1
+MBPSS2: bool = False  # Фильтр MB-PSS на входе 2
+
+Vstmax: str = 'Vstmax'  # Максимальное ограничение выхода стабилизатора
+Vstmin: str = 'Vstmin'  # Минимальное ограничение выхода стабилизатора
+Vlmax: str = 'Vlmax'  #
+Vlmin: str = 'Vlmin'  #
+Vimax: str = 'Vimax'  #
+Vimin: str = 'Vimin'  #
+Vvlmax: str = 'Vvlmax'  #
+Vvlmin: str = 'Vvlmin'  #
+Vhmax: str = 'Vhmax'  #
+Vhmin: str = 'Vhmin'  #
+
+KL: str = 'KL'  #
+KL1: str = 'KL1'  #
+KL2: str = 'KL2'  #
+KL11: str = 'KL11'  #
+KL17: str = 'KL11'  #
+
+KI: str = 'KI'  #
+KI1: str = 'KI1'  #
+KI2: str = 'KI2'  #
+
+KI11: str = 'KI11'  #
+KI17: str = 'KI17'  #
+
+KH: str = 'KH'  #
+KH1: str = 'KH1'  #
+KH2: str = 'KH2'  #
+KH11: str = 'KH11'  #
+KH17: str = 'KH17'  #
+
+TL1: str = 'TL1'  #
+TL2: str = 'TL2'  #
+TL3: str = 'TL3'  #
+TL4: str = 'TL4'  #
+TL5: str = 'TL5'  #
+TL6: str = 'TL6'  #
+TL7: str = 'TL7'  #
+TL8: str = 'TL8'  #
+TL9: str = 'TL9'  #
+TL10: str = 'TL10'  #
+TL11: str = 'TL11'  #
+TL12: str = 'TL12'  #
+
+TI1: str = 'TI1'  #
+TI2: str = 'TI2'  #
+TI3: str = 'TI3'  #
+TI4: str = 'TI4'  #
+TI5: str = 'TI5'  #
+TI6: str = 'TI6'  #
+TI7: str = 'TI7'  #
+TI8: str = 'TI8'  #
+TI9: str = 'TI9'  #
+TI10: str = 'TI10'  #
+TI11: str = 'TI11'  #
+TI12: str = 'TI12'  #
+
+TH1: str = 'TH1'  #
+TH2: str = 'TH1'  #
+TH3: str = 'TH1'  #
+TH4: str = 'TH4'  #
+TH5: str = 'TH5'  #
+TH6: str = 'TH6'  #
+TH7: str = 'TH7'  #
+TH8: str = 'TH8'  #
+TH9: str = 'TH9'  #
+TH10: str = 'TH10'  #
+TH11: str = 'TH11'  #
+TH12: str = 'TH12'  #
