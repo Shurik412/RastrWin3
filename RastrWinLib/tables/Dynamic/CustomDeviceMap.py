9069c68569586eb9a5ead865eaa16d0eb429e570
ohrimenko_ag <shurik412@mail.ru>
2021-04-26 12:50:23 +0300

    Add modules CustomDeviceMap.py

---
+++ RastrWinLib/tables/Dynamic/CustomDeviceMap.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Модуль переменных таблицы "Пользовательсие устройства" RastrWin3
+
+table: str = 'CustomDeviceMap'  # название таблиц
+
+Id: str = 'Id'  # Номер
+Name: str = 'Name'  # название
+Module: str = 'Module'  # Модуль пользовательского устройства
+InstanceTable: str = 'InstanceTable'  # Таблица экземпляров пользовательского устройства
+Tag: str = 'Tag'  # Тег связи с внешними устройствами
+LinkList: str = 'LinkList'  # Связи 1  Связи с ведомыми
+SiblingLinkList: str = 'SiblingLinkList'  # Связи 2 Связи с ведущими
+
