8312cddf236cd9c7cda4ab9d3d0a134ef81f8cf4
ohrimenko_ag <shurik412@mail.ru>
2021-06-04 09:15:54 +0300

    Add documentation in DFW421UEL_Filling.py

--- Eurostag/create_bat_file.py
+++ Eurostag/create_bat_file.py
@@ -5,8 +5,8 @@ DIR_EUROSTAG: str = r'C:\Eurostag44\Eurostag'
 DIR_HOME_BAT: str = r'C:\02_26_ZimaMin'
 NAME_SEZON: str = 'ZimaMin'  # ((1) ZimaMax - Зима макс, (2) ZimaMin - Зима мин, (3) LetoMax - Лето макс, (4) LetoMax - Лето мин.
 KEY_Y_N: str = 'N'  # Y - с PSS, N - без PSS
-U_NOM = 220  # номинальное напряжение в точке КЗ
-NAME_DTA_FILE: str = '7'  # название dta файла
+U_NOM = 500  # номинальное напряжение в точке КЗ
+NAME_DTA_FILE: str = '6'  # название dta файла
 COUNT_REGIME: int = 57
 
 if NAME_SEZON == 'ZimaMax':
@@ -14,6 +14,7 @@ if NAME_SEZON == 'ZimaMax':
     file.write(f'set EUROSTAG={DIR_EUROSTAG}\nset EDITOR=%windir%\\system32\\notepad.exe\nset HOME={DIR_HOME_BAT}\n\n')
     for i in range(1, COUNT_REGIME+1):
         file.write(f'eustag_cpt -s 1_{i}_{KEY_Y_N}_{U_NOM}kV.seq {NAME_DTA_FILE}.dta 1_{i}.sav\n')
+    file.write('pause')
     file.close()
 
 elif NAME_SEZON == 'ZimaMin':
@@ -21,6 +22,7 @@ elif NAME_SEZON == 'ZimaMin':
     file.write(f'set EUROSTAG={DIR_EUROSTAG}\nset EDITOR=%windir%\\system32\\notepad.exe\nset HOME={DIR_HOME_BAT}\n')
     for i in range(1, COUNT_REGIME+1):
         file.write(f'eustag_cpt -s 2_{i}_{KEY_Y_N}_{U_NOM}kV.seq {NAME_DTA_FILE}.dta 2_{i}.sav\n')
+    file.write('pause')
     file.close()
 
 elif NAME_SEZON == 'LetoMax':
@@ -28,6 +30,7 @@ elif NAME_SEZON == 'LetoMax':
     file.write(f'set EUROSTAG={DIR_EUROSTAG}\nset EDITOR=%windir%\\system32\\notepad.exe\nset HOME={DIR_HOME_BAT}\n')
     for i in range(1, COUNT_REGIME+1):
         file.write(f'eustag_cpt -s 3_{i}_{KEY_Y_N}_{U_NOM}kV.seq {NAME_DTA_FILE}.dta 3_{i}.sav\n')
+    file.write('pause')
     file.close()
 
 elif NAME_SEZON == 'LetoMin':
@@ -35,6 +38,7 @@ elif NAME_SEZON == 'LetoMin':
     file.write(f'set EUROSTAG={DIR_EUROSTAG}\nset EDITOR=%windir%\\system32\\notepad.exe\nset HOME={DIR_HOME_BAT}\n')
     for i in range(1, COUNT_REGIME+1):
         file.write(f'eustag_cpt -s 4_{i}_{KEY_Y_N}_{U_NOM}kV.seq {NAME_DTA_FILE}.dta 4_{i}.sav\n')
+    file.write('pause')
     file.close()
 
 print('The End')
