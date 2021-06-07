982c36da4241cc38eabb88cf2a4061242952af4a
ohrimenko_ag <shurik412@mail.ru>
2021-06-07 16:25:22 +0300

    Add testing code FuncPQ_Filling.py

--- RastrWinLib/FillingTable/FuncPQ_Filling.py
+++ RastrWinLib/FillingTable/FuncPQ_Filling.py
@@ -16,27 +16,55 @@ def filling_FuncPQ(
 ):
     """
 
-    :param rastr_win:
-    :param row_id:
-    :param Id:
-    :param P:
-    :param Q:
-    :param switch_command_line:
+    :param rastr_win: COM - объект Rastr.Astra (win32com);
+    :param row_id: Порядковый номер в таблице: возвращается функцией FindNexSel;
+    :param Id: Номер Х-ки [N];
+    :param P: P [P];
+    :param Q: Q [Q];
+    :param switch_command_line: True/False - вывод сообщений в протокол;
     :return:
     """
     variable_ = Variable(rastr_win=rastr_win)
 
-    variable_.make_changes_row(table=FuncPQ.table,
-                               column=FuncPQ.Id,
-                               row_id=row_id,
-                               value=Id)
+    variable_.make_changes_filling_row(table=FuncPQ.table,
+                                       column=FuncPQ.Id,
+                                       row_id=row_id,
+                                       value=Id)
 
-    variable_.make_changes_row(table=FuncPQ.table,
-                               column=FuncPQ.P,
-                               row_id=row_id,
-                               value=P)
+    variable_.make_changes_filling_row(table=FuncPQ.table,
+                                       column=FuncPQ.P,
+                                       row_id=row_id,
+                                       value=P)
 
-    variable_.make_changes_row(table=FuncPQ.table,
-                               column=FuncPQ.Q,
-                               row_id=row_id,
-                               value=Q)
+    variable_.make_changes_filling_row(table=FuncPQ.table,
+                                       column=FuncPQ.Q,
+                                       row_id=row_id,
+                                       value=Q)
+
+
+if __name__ == '__main__':
+    from RastrWinLib.AstraRastr import RASTR
+    from RastrWinLib.loading.load import load_file
+    from RastrWinLib.loading.save import save_file
+    from RastrWinLib.loading.shablon import Shabl
+    from RastrWinLib.tables.Dynamic.FuncPQ import FuncPQ
+
+    load_file(rastr_win=RASTR,
+              file_path=r'C:\Users\Ohrimenko_AG\Desktop\test_filling\test.rst',
+              shabl=Shabl.shablon_file_dynamic)
+
+    table_ = RASTR.Tables(FuncPQ.table)
+
+    for i in range(0, 100):
+        table_.AddRow()
+        filling_FuncPQ(
+            rastr_win=RASTR,
+            row_id=i,
+            Id=123123123,
+            P=12,
+            Q=14,
+            switch_command_line=False)
+
+    save_file(rastr_win=RASTR,
+              file_path=r'C:\Users\Ohrimenko_AG\Desktop\test_filling\test_FuncPQ.rst',
+              shabl=Shabl.shablon_file_dynamic)
