31b3068bac399ff4a5831f6f0900a3ca8510109b
ohrimenko_ag <shurik412@mail.ru>
2021-10-26 15:07:38 +0300

    correcting file tools.py

--- RastrWinLib/Tools/tools.py
+++ RastrWinLib/Tools/tools.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import time
+from functools import wraps
+
 from prettytable import PrettyTable
 
 
@@ -34,7 +37,27 @@ class TableOutput(PrettyTable):
         print(self.get_string(title=title_table))
 
 
+def timethis(func):
+    """
+    Декоратор для определения веремени работы функции.
+    :param func: любая функция
+    :return:
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(
+            f'Название функции: {func.__name__}; время работы: {changing_number_of_semicolons(number=(end - start), digits=3)} сек.')
+        return result
+
+    return wrapper
+
+
 if __name__ == '__main__':
+    # Testing class TableOutput
     tb = TableOutput(fieldName=['Описание', 'Параметр'])
     tb.row_add(['Время расчета для динамики', 'cек.'])
     tb.row_add(['Время расчета для динамики', 'cек.'])
@@ -42,4 +65,21 @@ if __name__ == '__main__':
     tb.row_add(['Время расчета для динамики', 'cек.'])
     tb.show(title_table='Параметры')
 
+    # Testing function changing_number_of_semicolons
     print(changing_number_of_semicolons(number=15315.00515, digits=5))
+
+
+    # Testing @timethis
+    @timethis
+    def countdown(n):
+        """
+
+        :param n:
+        :return:
+        """
+        while n > 0:
+            # print(n)
+            n = n - 1
+
+
+    countdown(1000000)
