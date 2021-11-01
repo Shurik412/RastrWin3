1aeea57e8c95c1ab9da14fe76d542dbe0699283a
ohrimenko_ag <shurik412@mail.ru>
2021-11-01 11:42:38 +0300

    Correction Save.py function save_file

--- RastrWin3/Save.py
+++ RastrWin3/Save.py
@@ -17,13 +17,49 @@ def save_file(rastr_win=RASTR,
     :param switch_command_line: True/False - выводит сообщения в протокол;
     :return: None;
     """
-    rastr_win.Save(path_file, directory_shabl(rus_name_shabl=shabl))
 
-    if switch_command_line:
-        pt = TableOutput(fieldName=['Файл', 'Шаблон'])
-        pt.title = 'Сохранение файла RastrWin3'
-        if path_file == '' or path_file == ' ' or path_file is None:
-            pt.row_add(['Ссылка для сохранения файла не задана!', shabl])
+    def save(path_file_save: str, shablon: str) -> bool:
+        try:
+            rastr_win.Save(path_file_save, shablon)
+        except pywintypes.com_error as er:
+            hr, msg, exc, arg = er.args
+            table_output = TableOutput(fieldName=['Сообщение'])
+            table_output.row_add(message=[exc[2]])
+            table_output.show(title_table=f'Ошибка при запуске функции: "{save_file.__name__}"')
+            return False
+        except pythoncom.com_error as error:
+            hr, msg, exc, arg = error.args
+            table_output = TableOutput(fieldName=['Сообщение'])
+            table_output.row_add(message=[exc[2]])
+            table_output.show(title_table=f'Ошибка при запуске функции: "{save_file.__name__}"')
+            return False
+        except ValueError as error:
+            table_output = TableOutput(fieldName=['Сообщение'])
+            table_output.row_add(message=[f'Тип аргумента "rg_kod" должен быть int а не "";\n {error}'])
+            table_output.show(title_table=f'Ошибка при запуске функции: "{save_file.__name__}"')
+            return False
         else:
-            pt.row_add([f'{path_file}', shabl])
-        pt.show(title_table='Сохранение файла RastrWin3')
+            return True
+
+    if shabl is None or shabl == '' or shabl == ' ':
+        save_ = save(path_file_save=path_file, shablon="")
+        if save_:
+            shabl = 'без шаблона \n по-умолчанию выбран: "режим"'
+            shabl_rgm = directory_shabl(rus_name_shabl='режим')
+            save(path_file_save=path_file, shablon=shabl_rgm)
+            if switch_command_line:
+                pt = TableOutput(fieldName=['Файл', 'Шаблон'])
+                pt.title = 'Сохранение файла RastrWin3'
+                if path_file == '' or path_file == ' ' or path_file is None:
+                    pt.row_add(['Ссылка для сохранения файла не задана!', shabl])
+                else:
+                    pt.row_add([path_file, shabl])
+                pt.show(title_table='Сохранение файла RastrWin3')
+    else:
+        shabl_path_file = directory_shabl(rus_name_shabl=shabl)
+        load = load_(kod_rg=rg_kod, pathFile=path_file, shablon=shabl_path_file)
+        if load:
+            if switch_command_line:
+                pt = TableOutput(fieldName=['Файл', 'Шаблон'])
+                pt.add_row([path_file, shabl_path_file])
+                pt.show(title_table='Сохранение файла RastrWin3')
